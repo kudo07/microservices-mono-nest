354744fc39d1bbf1d612a980bc282bbf6faab2c0
kudo07 <vknwl077@gmail.com>
2026-04-05 02:50:41 +0530

    feat:auth-service

--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,12 +1,25 @@
+import { DatabaseService, users } from '@app/database';
 import { KAFKA_SERVICE } from '@app/kafka';
 import { KAFKA_TOPICS } from '@app/kafka/constants/kafka.constants';
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  OnModuleInit,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import { eq } from 'drizzle-orm';
+
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthServiceService implements OnModuleInit {
   constructor(
     @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+    private readonly dbService: DatabaseService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async onModuleInit() {
@@ -15,9 +28,9 @@ export class AuthServiceService implements OnModuleInit {
   }
 
   // this is for testing the route
-  // getHello(): string {
-  //   return 'Hello World!';
-  // }
+  getHello(): string {
+    return 'Hello World!';
+  }
 
   // simulateUserRegistrationg(email: string) {
   //   // publist event to kafka
@@ -28,4 +41,84 @@ export class AuthServiceService implements OnModuleInit {
   //   });
   //   return { message: `User registered: ${email}` };
   // }
+
+  async register(email: string, password: string, name: string) {
+    // check if user existg
+
+    const existingUser = await this.dbService.db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (existingUser.length > 0) {
+      throw new ConflictException('User already exists');
+    }
+
+    // hashed Password
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [user] = await this.dbService.db
+      .insert(users)
+      .values({ email, password: hashedPassword, name })
+      .returning();
+
+    // send user registered event
+
+    this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
+      userId: user.id,
+      email: user.email,
+      name: user.name,
+      timestamp: new Date().toISOString(),
+    });
+
+    return { message: 'User registered successfully', userId: user.id };
+  }
+
+  async login(email: string, password: string) {
+    const [user] = await this.dbService.db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const token = this.jwtService.sign({ sub: user.id, email: user.email });
+
+    this.kafkaClient.emit(KAFKA_TOPICS.USER_LOGIN, {
+      userId: user.id,
+      timestamp: new Date().toISOString(),
+    });
+
+    return {
+      access_token: token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+    };
+  }
+  async getProfile(userId: string) {
+    const [user] = await this.dbService.db
+      .select({
+        id: users.id,
+        email: users.email,
+        name: users.name,
+        role: users.role,
+      })
+      .from(users)
+      .where(eq(users.id, userId))
+      .limit(1);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return user;
+  }
 }
