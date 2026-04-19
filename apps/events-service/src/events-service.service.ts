96bf17a9f40122886ffda77b4ab8cf3ff0c934ff
kudo07 <vknwl077@gmail.com>
2026-04-19 18:44:18 +0530

    feat:event-service

--- apps/events-service/src/events-service.service.ts
+++ apps/events-service/src/events-service.service.ts
@@ -1,8 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { CreateEventDto, UpdateEventDto } from '@app/common';
+import { DatabaseService } from '@app/database';
+import { events } from '@app/database/schema/events';
+import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
+import {
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class EventsServiceService {
+  constructor(
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+    private readonly dbService: DatabaseService,
+  ) {}
+
+  async onModuleInit() {
+    // connect to kafka when module intializes
+    await this.kafkaClient.connect();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  async create(createEventDto: CreateEventDto, organizerId: string) {
+    const [event] = await this.dbService.db
+      .insert(events)
+      .values({
+        ...createEventDto,
+        date: new Date(createEventDto.date),
+        price: createEventDto.price || 0,
+        organizerId,
+      })
+      .returning();
+
+    this.kafkaClient.emit(KAFKA_TOPICS.EVENT_CREATED, {
+      eventId: event.id,
+      organizerId: event.organizerId,
+      title: event.title,
+      timestamp: new Date().toISOString(),
+    });
+    return event;
+  }
+
+  async findOne(id: string) {
+    const [event] = await this.dbService.db
+      .select()
+      .from(events)
+      .where(eq(events.id, id))
+      .limit(1);
+
+    if (!event) {
+      throw new NotFoundException('Event not found');
+    }
+
+    return event;
+  }
+
+  async update(
+    id: string,
+    updateEventDto: UpdateEventDto,
+    userId: string,
+    userRole: string,
+  ) {
+    const event = await this.findOne(id);
+
+    if (event.organizerId !== userId && userRole !== 'ADMIN') {
+      throw new ForbiddenException(
+        'You are not authorized to update this event',
+      );
+    }
+
+    const updatedData: Record<string, unknown> = { ...updateEventDto };
+    if (updateEventDto.date) {
+      updatedData.date = new Date(updateEventDto.date);
+    }
+    updatedData.updatedAt = new Date();
+
+    const [updated] = await this.dbService.db
+      .update(events)
+      .set(updatedData)
+      .where(eq(events.id, id))
+      .returning();
+    this.kafkaClient.emit(KAFKA_TOPICS.EVENT_UPDATED, {
+      eventId: updated.id,
+      changes: Object.keys(updateEventDto),
+      timestamp: new Date().toISOString(),
+    });
+    return updated;
+  }
+
+  async publish(id: string, userId: string, userRole: string) {}
 }
--- apps/events-service/src/main.ts
+++ apps/events-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsServiceModule } from './events-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SERVICES_PORTS } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(SERVICES_PORTS.EVENTS_SERVICE);
+  console.log(
+    `Events Service is running on port ${SERVICES_PORTS.EVENTS_SERVICE}`,
+  );
 }
 bootstrap();
