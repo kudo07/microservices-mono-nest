daee4a2cdeeed4b809b81c556279485ad272cb87
kudo07 <vknwl077@gmail.com>
2026-04-29 01:48:19 +0530

    feat:

--- libs/common/src/interfaces/response.interface.ts
+++ libs/common/src/interfaces/response.interface.ts
@@ -10,3 +10,17 @@ export interface UserProfileResponse {
 export interface AuthResponse {
   accessToken: string;
 }
+
+export interface EventResponse {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  location: string;
+  capacity: number;
+  price: number;
+  status: string;
+  organizerId: string;
+  createdAt: string;
+  updatedAt: string;
+}
